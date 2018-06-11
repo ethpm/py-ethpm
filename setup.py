a4fd6092c1e9793d418b08283863fb7cab2414d2
Nick Gheorghita <nickgheorghita@gmail.com>
2018-06-11 12:44:22 -0600

    Bump version: 0.1.0-alpha.8 → 0.1.0-alpha.9

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ extras_require['dev'] = (
 setup(
     name='eth-pm',
     # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
-    version='0.1.0-alpha.8',
+    version='0.1.0-alpha.9',
     description="""Python abstraction for ERC190 packages.""",
     long_description_markdown_filename='README.md',
     author='Piper Merriam',
