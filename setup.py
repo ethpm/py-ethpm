9d0ced3e9c05af13af8e425341b56aa5d05f69f2
Nick Gheorghita <nickgheorghita@gmail.com>
2018-06-29 09:40:14 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ extras_require['dev'] = (
 setup(
     name='ethpm',
     # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
-    version='0.1.0-alpha.20',
+    version='0.1.0-alpha.21',
     description="""Python abstraction for ERC190 packages.""",
     long_description_markdown_filename='README.md',
     author='Piper Merriam',
