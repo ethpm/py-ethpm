3910365422c5eb99a22de3e2305f545fc45ee4e0
Nick Gheorghita <nickgheorghita@gmail.com>
2019-01-03 17:01:27 +0100

    Remove pyevmbackend from w3 fixture

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ readme = open(os.path.join(DIR, 'README.md')).read()
 extras_require={
     'test': [
         'pytest>=3.2.1,<4',
+        'pytest-ethereum>=0.1.3a.6,<1',
         'tox>=1.8.0,<2',
     ],
     'lint': [
@@ -29,6 +30,8 @@ extras_require={
         'sphinx_rtd_theme>=0.1.9,<2',
     ],
     'dev': [
+        'bumpversion>=0.5.3,<1',
+        'ipython>=7.2.0,<8',
         'pytest-watch>=4.1.0,<5',
         'twine',
         'wheel',
@@ -53,18 +56,13 @@ setup(
     url='https://github.com/ethpm/py-ethpm',
     include_package_data=True,
     install_requires=[
-        'bumpversion>=0.5.3,<1',
-        'eth-keys>=0.2.0b3,<1',
-        'eth-utils>=1.3.0,<2',
-        'eth-tester>=0.1.0b34,<1',
+        'eth-utils>=1.4.1,<2',
         'ipfsapi>=0.4.3,<1',
         'jsonschema>=2.6.0,<3',
         'protobuf>=3.0.0,<4',
-        'pytest-ethereum>=0.1.3a.6,<1',
-        'py-evm>=0.2.0a38,<1',
         'py-solc>=3.2.0,<4',
         'rlp>=1.0.1,<2',
-        'web3[tester]>=5.0.0a2,<6.0.0',
+        'web3[tester]>=5.0.0a3,<6',
     ],
     setup_requires=['setuptools-markdown'],
     python_requires='>=3.6, <4',
